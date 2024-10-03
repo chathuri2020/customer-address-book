dec6b539246bb251767c48c688e5ff897e4e2230
chathuri2020 <chathu9935@gmail.com>
2024-10-03 22:49:50 +0530

    web routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProjectController;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');  // Giving the route a name 'welcome'
+
+Route::resource('customers', CustomerController::class);
+Route::resource('projects', ProjectController::class);
 
 Route::get('/', function () {
     return view('welcome');
