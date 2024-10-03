9877c4ea5549cd1980eb15f4ded4325679f01e7c
chathuri2020 <chathu9935@gmail.com>
2024-10-03 22:38:02 +0530

    authentications

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome');  // Giving the route a name 'welcome'
+});
 
-Route::resource('customers', CustomerController::class);
-Route::resource('projects', ProjectController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
